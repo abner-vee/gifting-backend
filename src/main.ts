ed78b4151f2dabc19182dba1b59919b545707704
Vivian Abner <abnervivian@gmail.com>
2025-02-28 13:41:40 +0100

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,17 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
-    })
+    }),
   );
   app.enableCors({
     origin: ['http://localhost:3000', 'http://localhost:3001'],
