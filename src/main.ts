d22b8f2a57a8728452eeddf4dac0d82c25095969
Shadrach Adamu <shadrachadamuul@gmail.com>
2025-02-28 20:41:19 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: ['https://gifting-frontend-kpgr.onrender.com', 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Allow cookies
   });
