b966063c3748510bcd6d042b63c8707823d41f4e
Shadrach Adamu <shadrachadamuul@gmail.com>
2025-02-28 14:34:55 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.useGlobalPipes(
     new ValidationPipe({
